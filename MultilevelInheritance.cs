3383f3d7d0abe6151659694c7fc886060e6bd1d3
Akshay.Sharma <akshay.sharma@oneadvanced.com>
2022-10-31 02:33:30 +0530

    Add Multilevel Inheritance

---
+++ MultilevelInheritance.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OopsProject
+{
+    public class Country
+    {
+        public void CountryName()
+        {
+            Console.WriteLine("India");
+        }
+    }
+
+    public class State : Country
+    {
+        public void StateName()
+        {
+            Console.WriteLine("Gujarat");
+        }
+    }
+
+    public class City : State
+    {
+        public void CityName()
+        {
+            Console.WriteLine("Ahmedabad");
+        }
+    }
+    internal class MultilevelInheritance
+    {
+        public static void Main(string[] args)
+        {
+            City myCity = new City();
+            myCity.CountryName();
+            myCity.StateName();
+            myCity.CityName();
+        }
+    }
+}
