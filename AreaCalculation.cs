3c62a4365afc5f877c5f9226446a444da962c4b6
Akshay.Sharma <akshay.sharma@oneadvanced.com>
2022-10-31 01:24:24 +0530

    Add FruitDetails

---
+++ AreaCalculation.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OopsProject
+{
+    class Shape
+    {
+        protected int length, breadth;
+
+        public Shape( int lengthInput = 0, int breadthInput = 0)
+        {
+            length = lengthInput;
+            breadth = breadthInput;
+        }
+        public virtual int Area()
+        {
+            Console.WriteLine("Parent class area: ");
+            return 0;
+        }
+    }
+
+    class Rectangle : Shape
+    {
+        public Rectangle(int lengthInput = 0, int breadthInput = 0): base(lengthInput, breadthInput)
+        {
+
+        }
+
+        public override int Area()
+        {
+            return (length * breadth);
+        }
+    }
+
+    class Square : Shape
+    {
+        public Square(int lengthInput = 0, int breadthInput = 0) : base(lengthInput, breadthInput)
+        {
+            
+        }
+
+        public override int Area()
+        {
+            return (length * breadth);
+        }
+    }
+    internal class AreaCalculation
+    {
+        static void Main(string[] args)
+        {
+            Rectangle r = new(10,5);
+            Square s = new (10, 10);
+            Console.WriteLine("Area of Rectangle is " + r + ".");
+            Console.WriteLine("Area of Square is " + s + ".");
+        }
+    }
+}
---
+++ FruitDetails.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OopsProject
+{
+    //Fruit Interface
+    interface IFruit
+    {
+        void FruitName(); //interface method (does not have a body)
+        void FruitColor(); //interface method (does not have a body)
+    }
+
+    //Apple implements the IFruit interface
+    class Apple : IFruit
+    {
+       public void FruitName()
+        {
+            //The body of FruitName() is provided here
+            Console.WriteLine("Fruit Name: Apple");
+        }
+        
+        public void FruitColor()
+        {
+            // The body of FruitColor() is provided here
+            Console.WriteLine("Fruit Color: Red");
+            Console.WriteLine();
+        }
+    }
+
+    class Mango : IFruit
+    {
+        public void FruitName()
+        {
+            //The body of FruitName() is provided here
+            Console.WriteLine("Fruit Name: Mango");
+        }
+
+        public void FruitColor()
+        {
+            // The body of FruitColor() is provided here
+            Console.WriteLine("Fruit Color: Yellow");
+            Console.WriteLine();
+        }
+    }
+
+    class Papaya : IFruit
+    {
+        public void FruitName()
+        {
+            //The body of FruitName() is provided here
+            Console.WriteLine("Fruit Name: Papaya");
+        }
+
+        public void FruitColor()
+        {
+            // The body of FruitColor() is provided here
+            Console.WriteLine("Fruit Color: Yellow");
+            Console.WriteLine();
+        }
+    }
+    internal class FruitDetails
+    {
+        static void Main(string[] args)
+        {
+            Apple myApple = new Apple(); //Create an Apple object
+            myApple.FruitName();
+            myApple.FruitColor();
+
+            Mango myMango = new Mango(); //Create an Apple object
+            myMango.FruitName();
+            myMango.FruitColor();
+
+            Papaya myPapaya = new Papaya(); //Create an Apple object
+            myPapaya.FruitName();
+            myPapaya.FruitColor();
+        }
+        
+    }
+}
