d0d18b59f8f5bf8c4a5cdcc51016c0c2a3cbc835
Akshay.Sharma <akshay.sharma@oneadvanced.com>
2022-10-31 02:48:04 +0530

    Add HierarchicalInheritance

---
+++ HierarchicalInheritance.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OopsProject
+{
+    public class Father
+    {
+        public string FatherName()
+        {
+            return "Mohit";
+        }
+    }
+
+    public class Son : Father
+    {
+        public string SonName()
+        {
+            return "Ali";
+        }
+    }
+
+    public class Daughter: Father
+    {
+        public string DaughterName()
+        {
+            return "Aayesha";
+        }
+    }
+    internal class HierarchicalInheritance
+    {
+        public static void Main(string[] args)
+        {
+            Son son = new Son();
+            Console.WriteLine("My name is " + son.SonName() + " and my father's name is " + son.FatherName() + ".");
+            Daughter daughter = new Daughter();
+            Console.WriteLine("My name is " + daughter.DaughterName() + " and my father's name is " + daughter.FatherName() + ".");
+        }
+    }
+}
