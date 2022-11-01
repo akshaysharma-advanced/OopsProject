137df666a00ef4a0893eda1036b9509f47767714
Akshay.Sharma <akshay.sharma@oneadvanced.com>
2022-11-01 12:00:09 +0530

    Add AccessModifiers File

---
+++ AccessModifiers.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OopsProject
+{
+    class SimpleInterest
+    {
+        //public member variables
+        public int principalAmount;
+        public double rateOfInterest;
+        public int years;
+
+        public double CalculateInterest()
+        {
+            return principalAmount * rateOfInterest * years;
+        }
+    }
+    internal class AccessModifiers
+    {
+        static void Main(string[] args)
+        {
+            SimpleInterest simpleInterest = new SimpleInterest();
+            simpleInterest.principalAmount = 2000;
+            simpleInterest.rateOfInterest = 0.1;
+            simpleInterest.years = 3;
+            Console.WriteLine("Simple Interest:" + simpleInterest.CalculateInterest());
+        }
+    }
+}
